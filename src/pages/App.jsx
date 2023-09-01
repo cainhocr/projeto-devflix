f4bd41f68dedbd45370be9fc4337c5d77f4b0bb5
cainhocr <caioignacio086@gmail.com>
2023-09-01 13:11:19 -0300

    pr finalizado

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
           <div className="linkMenu">
             <ul>
               <li>
-                <a href="#">inicio</a>
+                <a href="#">Inicio</a>
               </li>
               <li>
                 <a href="#">Filmes</a>
